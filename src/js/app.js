5db61096e5ec75d658a1ff9c53e65051a5c3fbd4
odinokun <odinokun@gmail.com>
2018-09-02 13:19:24 +0300

    add ыÐыÐsvg mixin & no-js class

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,9 @@ $(function() {
   // begin SVG for IE
   require('./modules/svg4everybody')();
 
+  // begin no-JS
+  require('./modules/noJs')();
+
   // begin Scroll2Ancor
   // require('./modules/scroll2ancor')();
 
---
+++ src/js/modules/noJs.js
@@ -0,0 +1,7 @@
+module.exports = function() {
+
+  // begin remove no-js class
+  $('.main-wrap').removeClass('no-js');
+  // end remove no-js class
+
+};
