c24068707079feeac3e979423e66d4757746589c
odinokun <odinokun@gmail.com>
2023-01-14 20:34:36 +0100

    delete swiper & update styles

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -3,15 +3,9 @@
 module.exports = [
   './node_modules/normalize.css/normalize.css'
 
-  // include jQuery UI
   // './src/vendors/jquery-ui/jquery-ui.min.css',
 
-  // include Slick slider
-  // './node_modules/slick-carousel/slick/slick.css',
-
-  // include animate.css
   // './node_modules/animate.css/animate.css',
 
-  // include fancybox3
   // './node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
 ];
--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -4,15 +4,9 @@ module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
   './node_modules/svg4everybody/dist/svg4everybody.min.js'
 
-  // include jQuery UI
   // './src/vendors/jquery-ui/jquery-ui.min.js',
 
-  // include Maskedinput
   // './src/vendors/maskedinput/jquery.maskedinput.min.js',
 
-  // include Slick slider
-  // './node_modules/slick-carousel/slick/slick.min.js',
-
-  // include fancybox3
   // './node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
 ];
