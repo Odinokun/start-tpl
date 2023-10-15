5a9d805c458a9cd30f8aacbc66c53cff4e6aa9fd
odinokun <odinokun@gmail.com>
2023-10-15 13:54:07 +0200

    upd: npm and scripts updated

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports = function() {
+  const sass = require('gulp-sass')(require('sass'));
 
   $.gulp.task('sass', function() {
     return $.gulp.src('./src/style/app.scss')
       .pipe($.gp.sourcemaps.init())
-      .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style SASS'}))
+      .pipe(sass()).on('error', $.gp.notify.onError({ title: 'Style SASS'}))
       .pipe($.gp.autoprefixer({grid: true}))
       .pipe($.gp.csso())
       .pipe($.gp.sourcemaps.write())
